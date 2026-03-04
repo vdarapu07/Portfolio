a218173659931e3bce4d62d0224c16d15d9a819c
vdarapu07 <vinoddarapu@icloud.com>
2026-03-04 17:46:32 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
@@ -35,3 +35,4 @@ export default function RootLayout({
     </html>
   );
 }
+
